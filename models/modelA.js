3c01d9c7c9bfb525f6057b8c580742819c2e5933
Eli Davidson <eli@shadowlab.dev>
2021-12-14 16:25:49 -0700

    Add modelA.js containing dinosaur object literal

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {                        // Declaire object literal
+  name: 'Littlefoot',                   // Declaire and assign name peramitor
+  color: 'Green',                       // Declaire and color name peramitor
+  age: 42,                              // Declaire and assign age number peramitor
+  weight: 374.6,                        // Declaire and weight float peramitor
+  isAlive: false,                       // Declaire and assign isAlive bool peramitor
+  isMissed: true,                       // Declaire and assign isMissed bool peramitor
+  diet: ['grass', 'trees', 'tacos'],    // Declaire and assign diet array
+  friends: ['Sara', 'Ducky', 'Spike']   // Declaire and assign friends array
+}                                       // Close dinosaur object literal
