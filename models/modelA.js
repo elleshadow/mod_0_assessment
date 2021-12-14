82d5137f94c33cb78c6aa0fde09a0147a5f78f39
Eli Davidson <eli@shadowlab.dev>
2021-12-14 16:36:15 -0700

    Add modelB.js containing developer object literal

--- models/modelA.js
+++ models/modelA.js
@@ -1,8 +1,8 @@
 var dinosaur = {                        // Declaire object literal
   name: 'Littlefoot',                   // Declaire and assign name peramitor
-  color: 'Green',                       // Declaire and color name peramitor
+  color: 'Green',                       // Declaire and assign color peramitor
   age: 42,                              // Declaire and assign age number peramitor
-  weight: 374.6,                        // Declaire and weight float peramitor
+  weight: 374.6,                        // Declaire and assign weight float peramitor
   isAlive: false,                       // Declaire and assign isAlive bool peramitor
   isMissed: true,                       // Declaire and assign isMissed bool peramitor
   diet: ['grass', 'trees', 'tacos'],    // Declaire and assign diet array
---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {                                     // Declaire object literal
+  name: 'Eli',                                        // Declaire and assign name peramitor
+  hairColor: 'Black',                                 // Declaire and assign hairColor peramitor
+  age: 33,                                            // Declaire and assign age number peramitor
+  powerLevel: 9001.6,                                 // Declaire and assign powerLevel float peramitor
+  isAwesome: true,                                    // Declaire and assign isAwesome bool peramitor
+  isTired: true,                                      // Declaire and assign isTired bool peramitor
+  codingLanguages: ['VB.NET', 'SQL', 'Javascript'],   // Declaire and assign codingLanguages array
+  spokenLanguages: ['Sara', 'Ducky', 'Spike']         // Declaire and assign spokenLanguages array
+}                                                     // Close developer object literal
