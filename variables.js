92c675d1fe82f385b58ff335fccdd344955fbc3f
Eli Davidson <eli@shadowlab.dev>
2021-12-14 16:11:53 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = 'The Matrix';
+var isEntertaining = true;
+movie = 'Princess Bride';
+var num4 = 7;
+var num6 = 3;
+var product = num4 * num6;
+var favoriteFoods = ['tacos', 'pizza', 'burritos'];
+favoriteFoods.shift();
+console.log(movie.length);
